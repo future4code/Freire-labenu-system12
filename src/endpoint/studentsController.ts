600e4cb835691eae4c8b1df4643cd5153b920fcd
Guilherme <guilherme.beutler@outlook.com>
2022-09-10 16:29:47 -0300

    Create, get and change students

--- src/endpoint/studentsController.ts
+++ src/endpoint/studentsController.ts
@@ -1,30 +1,117 @@
 import { Request, Response } from "express";
+import { uuid } from "uuidv4";
+import { StudentData } from "../data/StudentData";
+import { Student } from "../model/Student";
 
-class studentsController {
+export class StudentController {
 
-async createStudent(req:Request,res:Response){
-try {
-    const {name,email,birthdate,team_id}=req.body
-    const id = Date.now.toString()
-if(!name||!email||!birthdate||!team_id){
-    throw new Error("Todos os campos deve ser preenchidos");
+    async createStudent(req: Request, res: Response) {
+
+        try {
+
+            const { name, email, birth_date, team_id, hobbies } = req.body
+
+            if (!name || !email || !birth_date || !team_id || !hobbies) {
+                throw new Error('Nome, email, data de nascimento e turma são informações obrigatórias!')
+            }
+
+            const newDate = birth_date.split('/').reverse().join('-')
+
+            const id = uuid()
+
+            const newStudent = new Student(id, name, email, newDate, team_id, hobbies)
+
+            const studentDB = new StudentData()
+
+            await studentDB.insertStudent(newStudent)
+
+            res.status(201).send({ message: 'Estudente cadastrado com sucesso!' })
+
+        } catch (error: any) {
+            if (res.statusCode === 200) {
+                res.status(500).send({ message: error.message })
+            } else {
+                res.status(res.statusCode).send({ message: error.sqlMessage || error.message })
+            }
+        }
+    }
+
+    async getStudentByName(req: Request, res: Response) {
+
+        try {
+
+            const { name } = req.query
+
+            if (!name) {
+                throw new Error("O nome é obrigatório!");
+            }
+
+            const studentDB = new StudentData()
+
+            const student = await studentDB.getStudentByName(name as string)
+
+            res.status(200).send({ student })
+
+        } catch (error: any) {
+            if (res.statusCode === 200) {
+                res.status(500).send({ message: error.message })
+            } else {
+                res.status(res.statusCode).send({ message: error.sqlMessage || error.message })
+            }
+        }
+    }
+
+    async changeTeamStudent(req: Request, res: Response) {
+
+        try {
+
+            const { id } = req.params
+            const { team_id } = req.body
+
+            
+
+            if (!id || !team_id) {
+                throw new Error("Id e turma são informações obrigatórias!")
+            }
+
+            const studentDB = new StudentData()
+
+            await studentDB.updateTeamStudent(id, team_id)
+
+            console.log('ola')
+
+            res.status(200).send({ message: 'Estudante alterado com sucesso!' })
+
+        } catch (error: any) {
+            if (res.statusCode === 200) {
+                res.status(500).send({ message: error.message })
+            } else {
+                res.status(res.statusCode).send({ message: error.sqlMessage || error.message })
+            }
+        }
+    }
+
+    async getStudents(req: Request, res: Response) {
+
+        try {
+
+            const studentDB = new StudentData()
+
+            const student = await studentDB.selectStudents()
+
+            if (!student) {
+                res.statusCode = 404
+                throw new Error("Nenhum estudente encontrado")
+            }
+
+            res.status(200).send({ student })
+
+        } catch (error: any) {
+            if (res.statusCode === 200) {
+                res.status(500).send({ message: error.message })
+            } else {
+                res.status(res.statusCode).send({ message: error.sqlMessage || error.message })
+            }
+        }
+    }
 }
-// const student = new student(id,name,email,birthdate,team_id)
-
-
-
-} catch (error:any) {
-    res.status(500).send({message: error.sqlMessage || error.message})
-}
-
-
-
-
-}
-
-async getStudent(){
-
-}
-
-}
- export default studentsController
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { app } from "./app";
 import { InstructorController } from "./endpoint/InstructorController";
+import { StudentController } from "./endpoint/studentsController";
 import TeamController from "./endpoint/TeamController";
 
 const teamController = new TeamController()
 const instructorController = new InstructorController()
+const studentController = new StudentController()
 
 app.post('/team', teamController.createTeam)
 app.get('/team', teamController.getTeam)
@@ -13,4 +15,7 @@ app.post('/instructor', instructorController.createInstructor)
 app.get('/instructor', instructorController.getInstructor)
 app.put('/instructor/:id', instructorController.changeTeamInstructor)
 
-
+app.post('/student', studentController.createStudent)
+app.get('/student', studentController.getStudentByName)
+app.get('/student/all', studentController.getStudents)
+app.put('/student/:id', studentController.changeTeamStudent)
